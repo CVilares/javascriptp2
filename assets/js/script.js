4f27190cb0c7cfde45704465b0aa9eb90d81e1e7
Carlos vilares <car.vilares@gmail.com>
2023-04-24 22:53:42 +0000

    first question in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,10 @@
-let questions = [
-    {
-        question: "Which country won the 2018 FIFA World Cup?",
-        answers: ["France", "Brazil", "Germany", "Spain"],
-        correctAnswer: "France"
-    },
-    {
-        question: "Who won the Ballon d'Or in 2021?",
-        answers: ["Lionel Messi", "Cristiano Ronaldo", "Kylian Mbappé", "Robert Lewandowski"],
-        correctAnswer: "Lionel Messi"
-    }
-];
-
-let currentQuestion = 0;
-let score = 0;
-let timer;
-let timeLeft = 15;
-
-function displayQuestion() {
-    document.getElementById("question").innerHTML = questions[currentQuestion].question;
-    document.getElementById("answer1").innerHTML = questions[currentQuestion].answers[0];
-    document.getElementById("answer2").innerHTML = questions[currentQuestion].answers[1];
-    document.getElementById("answer3").innerHTML = questions[currentQuestion].answers[2];
-    document.getElementById("answer4").innerHTML = questions[currentQuestion].answers[3];
+let pergunta = {
+    titulo: 'qual a capital de portugal?'
+    alternativa:['lisbon','porto','ermesinde','coimbra'],
+    correcta: 0
+};
+function showquestion (q) {
+    let titleDiv= document.getElementById('titulo');
+    titleDiv.textContent= q.titulo;
 }
-
-function startTimer() {
-    timer = setInterval(() => {
-        timeLeft--;
-        document.getElementById("timer").innerHTML = `Time left: ${timeLeft}`;
-
-        if (timeLeft === 0) {
-            clearInterval(timer);
-            document.getElementById("answer1").disabled = true;
-            document.getElementById("answer2").disabled = true;
-            document.getElementById("answer3").disabled = true;
-            document.getElementById("answer4").disabled = true;
-            document.getElementById("next-btn").style.display = "block";
-            document.getElementById("timer").innerHTML = "Time's up!";
-        }
-    }, 1000);
-}
-
-function checkAnswer(selectedAnswer) {
-    if (selectedAnswer === questions[currentQuestion].correctAnswer) {
-        score++;
-        document.getElementById(selectedAnswer).style.backgroundColor = "green";
-    } else {
-        document.getElementById(selectedAnswer).style.backgroundColor = "red";
-        document.getElementById(questions[currentQuestion].correctAnswer).style.backgroundColor = "green";
-    }
-
-    document.getElementById("answer1").disabled = true;
-    document.getElementById("answer2").disabled = true;
-    document.getElementById("answer3").disabled = true;
-    document.getElementById("answer4").disabled = true;
-    document.getElementById("next-btn").style.display = "block";
-    clearInterval(timer);
-}
-
-function nextQuestion() {
-    currentQuestion++;
-    if (currentQuestion === questions.length) {
-        document.getElementById("quiz-wrapper").innerHTML = `You got ${score} out of ${questions.length} questions correct!`;
-    } else {
-        timeLeft = 15;
-        displayQuestion();
-        document.getElementById("timer").innerHTML = `Time left: ${
+showquestion(pergunta);
