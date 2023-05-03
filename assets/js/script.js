98c755f6f39219f82c26cfb23ec55b69aca8342d
Carlos vilares <car.vilares@gmail.com>
2023-05-03 14:51:43 +0000

    script src updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 let pergunta = {
     titulo: 'Gato',
-    alternativa: ['Gat','Cat','Gate','Dog'],
+    alternativas: ['Gat','Cat','Gate','Dog'],
     correcta: 1
 };
 
-function showquestion (q) {
+function mostraquestao(q) {
     let titleDiv = document.getElementById('titulo');
-    titleDiv.textContent= q.titulo;
+    titleDiv.textContent = q.titulo;
 
     let alts = document.querySelectorAll('.alternativa');
     alts.forEach(function(element,index){
-        element.textContent = q.alternativas [index];
+        element.textContent = q.alternativas[index];
         element.addEventListener('click', function(){
             if(index == q.correcta){
                 console.log("Acertou")
@@ -21,4 +21,4 @@ function showquestion (q) {
         })
     })
 }
-showquestion(pergunta);
+mostraquestao(pergunta);
