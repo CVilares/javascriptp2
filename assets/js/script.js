8ab8be479c5171ad2685dbde648c10fad7fc2db6
Carlos vilares <car.vilares@gmail.com>
2023-05-05 12:37:32 +0000

    Score function add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,83 @@
-let pergunta = {
-    titulo: 'Gato',
-    alternativas: ['Gat','Cat','Gate','Dog'],
-    correcta: 1
-};
-
-function mostraquestao(q) {
-    let titleDiv = document.getElementById('titulo');
-    titleDiv.textContent = q.titulo;
-
-    let alts = document.querySelectorAll('.alternativa');
-    alts.forEach(function(element,index){
+let perguntas = [
+    {
+      titulo: 'Gato',
+      alternativas: ['Gat', 'Cat', 'Gate', 'Dog'],
+      correcta: 1
+    },
+    {
+      titulo: 'pato',
+      alternativas: ['duck', 'Cat', 'Gate', 'Dog'],
+      correcta: 0
+    },
+    {
+      titulo: 'cao',
+      alternativas: ['Gat', 'Cat', 'Gate', 'Dog'],
+      correcta: 3
+    },
+    {
+      titulo: 'passaro',
+      alternativas: ['Gat', 'bird', 'Gate', 'Dog'],
+      correcta: 1
+    },
+    {
+      titulo: 'lagarto',
+      alternativas: ['Gat', 'lizard', 'Gate', 'Dog'],
+      correcta: 1
+    },
+    {
+      titulo: 'leao',
+      alternativas: ['Gat', 'Cat', 'lion', 'Dog'],
+      correcta: 2
+    }
+  ];
+  
+  let app = {
+    start: function () {
+      this.Atualpos = 0;
+      let alts = document.querySelectorAll('.alternativa');
+      alts.forEach((element, index) => {
+        element.addEventListener('click', () => {
+          this.checkAnswer(index);
+        });
+      });
+      this.mostraquestao(perguntas[this.Atualpos]);
+    },
+  
+    mostraquestao: function (q) {
+      this.qatual = q;
+      //showing title
+      let titleDiv = document.getElementById('titulo');
+      titleDiv.textContent = q.titulo;
+      //showing alternatives
+      let alts = document.querySelectorAll('.alternativa');
+      alts.forEach(function (element, index) {
         element.textContent = q.alternativas[index];
-        element.addEventListener('click', function(){
-            if(index == q.correcta){
-                console.log("Acertou")
-            }
-            else{
-                console.log("Errou")
-            }
-        })
-    })
-}
-mostraquestao(pergunta);
+      });
+    },
+  
+    nextquestion: function (user) {
+      this.Atualpos++;
+      if (this.Atualpos == perguntas.length) {
+        this.Atualpos = 0;
+      }
+    },
+  
+    checkAnswer: function (user) {
+      if (this.qatual.correcta == user) {
+        console.log('correcta');
+      } else {
+        console.log('Errada');
+      }
+      this.nextquestion();
+      this.mostraquestao(perguntas[this.Atualpos]);
+    }
+  },
+
+  updateScore: function () {
+    let scoreDiv = document.getElementById('score');
+    scoreDiv.textContent = 'Your score is : ${this.totalscore}'
+    
+  }
+  
+  app.start();
+  
