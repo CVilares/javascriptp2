646d01f01742deb27f3d7cd3c154139b9dee8141
Carlos vilares <car.vilares@gmail.com>
2023-05-05 15:12:24 +0000

    ading score result

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,85 @@
 let perguntas = [
-    {
-      titulo: 'Gato',
-      alternativas: ['Gat', 'Cat', 'Gate', 'Dog'],
-      correcta: 1
-    },
-    {
-      titulo: 'pato',
-      alternativas: ['duck', 'Cat', 'Gate', 'Dog'],
-      correcta: 0
-    },
-    {
-      titulo: 'cao',
-      alternativas: ['Gat', 'Cat', 'Gate', 'Dog'],
-      correcta: 3
-    },
-    {
-      titulo: 'passaro',
-      alternativas: ['Gat', 'bird', 'Gate', 'Dog'],
-      correcta: 1
-    },
-    {
-      titulo: 'lagarto',
-      alternativas: ['Gat', 'lizard', 'Gate', 'Dog'],
-      correcta: 1
-    },
-    {
-      titulo: 'leao',
-      alternativas: ['Gat', 'Cat', 'lion', 'Dog'],
-      correcta: 2
-    }
-  ];
+  {
+    titulo: 'Gato',
+    alternativas: ['Gat', 'Cat', 'Gate', 'Dog'],
+    correcta: 1
+  },
+  {
+    titulo: 'pato',
+    alternativas: ['duck', 'Cat', 'Gate', 'Dog'],
+    correcta: 0
+  },
+  {
+    titulo: 'cao',
+    alternativas: ['Gat', 'Cat', 'Gate', 'Dog'],
+    correcta: 3
+  },
+  {
+    titulo: 'passaro',
+    alternativas: ['Gat', 'bird', 'Gate', 'Dog'],
+    correcta: 1
+  },
+  {
+    titulo: 'lagarto',
+    alternativas: ['Gat', 'lizard', 'Gate', 'Dog'],
+    correcta: 1
+  },
+  {
+    titulo: 'leao',
+    alternativas: ['Gat', 'Cat', 'lion', 'Dog'],
+    correcta: 2
+  }
+];
+
+let app = {
+  totalscore: 0,
+  Atualpos: 0,
   
-  let app = {
-    start: function () {
+  start: function () {
+    let alts = document.querySelectorAll('.alternativa');
+    alts.forEach((element, index) => {
+      element.addEventListener('click', () => {
+        this.checkAnswer(index);
+      });
+    });
+    this.mostraquestao(perguntas[this.Atualpos]);
+  },
+
+  mostraquestao: function (q) {
+    this.qatual = q;
+    //showing title
+    let titleDiv = document.getElementById('titulo');
+    titleDiv.textContent = q.titulo;
+    //showing alternatives
+    let alts = document.querySelectorAll('.alternativa');
+    alts.forEach(function (element, index) {
+      element.textContent = q.alternativas[index];
+    });
+  },
+
+  nextquestion: function (user) {
+    this.Atualpos++;
+    if (this.Atualpos == perguntas.length) {
       this.Atualpos = 0;
-      let alts = document.querySelectorAll('.alternativa');
-      alts.forEach((element, index) => {
-        element.addEventListener('click', () => {
-          this.checkAnswer(index);
-        });
-      });
-      this.mostraquestao(perguntas[this.Atualpos]);
-    },
-  
-    mostraquestao: function (q) {
-      this.qatual = q;
-      //showing title
-      let titleDiv = document.getElementById('titulo');
-      titleDiv.textContent = q.titulo;
-      //showing alternatives
-      let alts = document.querySelectorAll('.alternativa');
-      alts.forEach(function (element, index) {
-        element.textContent = q.alternativas[index];
-      });
-    },
-  
-    nextquestion: function (user) {
-      this.Atualpos++;
-      if (this.Atualpos == perguntas.length) {
-        this.Atualpos = 0;
-      }
-    },
-  
-    checkAnswer: function (user) {
-      if (this.qatual.correcta == user) {
-        console.log('correcta');
-      } else {
-        console.log('Errada');
-      }
-      this.nextquestion();
-      this.mostraquestao(perguntas[this.Atualpos]);
     }
+  },
+
+  checkAnswer: function (user) {
+    if (this.qatual.correcta == user) {
+      console.log('correcta');
+      this.totalscore++;
+    } else {
+      console.log('Errada');
+    }
+    this.updateScore();
+    this.nextquestion();
+    this.mostraquestao(perguntas[this.Atualpos]);
   },
 
   updateScore: function () {
     let scoreDiv = document.getElementById('score');
-    scoreDiv.textContent = 'Your score is : ${this.totalscore}'
-    
+    scoreDiv.textContent = `Your score is: ${this.totalscore}`;
   }
-  
-  app.start();
-  
+};
+
+app.start();
